64aea5eb3aa03c185a08deec747e7db1a465515f
Truong Hong Thi <thi1981@gmail.com>
2020-03-19 02:07:27 +0700

    Add news.js

---
+++ news.js
@@ -0,0 +1,24 @@
+const { fetch } = require('./util')
+
+exports.getNews = () => {
+  const url = process.env.NEWS_URL
+  const hname = process.env.NEWS_HNAME
+  const hvalue = process.env.NEWS_HVALUE
+
+  return fetch(url, {
+      [hname]: hvalue
+    }).then(({ data }) => {
+    return data.data.reduce((list, item, index) => {
+      if (item.data && item.data.length) {
+        const info = item.data[0]
+        const date = new Date(info.created_at).toLocaleString('vi-VN')
+        const link = info.link
+        const text = `<b>${date}</b> <i>(tin số ${index + 1}/${data.data.length})</i>\n${item.title}\n\n<a href='${link}'>Mở link</a>`
+        list.push(text)
+      } else {
+        list.push(item.link_share)
+      }
+      return list
+    }, [])
+  })
+}
